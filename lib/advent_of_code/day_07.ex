cd703a47fbd4a3b9fab5751a8dd73e1403d86414
mrdotb <mrdotb@protonmail.com>
2023-12-07 17:30:16 +0100

    Shorten Day07

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,23 +1,9 @@
 defmodule AdventOfCode.Day07 do
   @signs ~w(2 3 4 5 6 7 8 9 T J Q K A)
-  @signs_map @signs
-             |> Enum.with_index(1)
-             |> Map.new()
+  @signs_map @signs |> Enum.with_index(1) |> Map.new()
 
   @signs2 ~w(J 2 3 4 5 6 7 8 9 T Q K A)
-  @signs2_map @signs2
-              |> Enum.with_index(1)
-              |> Map.new()
-
-  @hands %{
-    five: 7,
-    four: 6,
-    house: 5,
-    three: 4,
-    two: 3,
-    one: 2,
-    high: 1
-  }
+  @signs2_map @signs2 |> Enum.with_index(1) |> Map.new()
 
   defp parse_input(input) do
     input
@@ -32,22 +18,39 @@ defmodule AdventOfCode.Day07 do
     cards
     |> Enum.frequencies()
     |> Enum.map(fn {_, v} -> v end)
-    |> Enum.sort()
-    |> get_hand()
+    |> Enum.sort(:desc)
   end
 
-  defp get_hand([5]), do: :five
-  defp get_hand([1, 4]), do: :four
-  defp get_hand([2, 3]), do: :house
-  defp get_hand([1, 1, 3]), do: :three
-  defp get_hand([1, 2, 2]), do: :two
-  defp get_hand([1, 1, 1, 2]), do: :one
-  defp get_hand([1, 1, 1, 1, 1]), do: :high
+  defp kicker(signs_map, cards), do: Enum.map(cards, &Map.get(signs_map, &1))
 
-  defp kicker(signs_map, cards) do
-    Enum.map(cards, fn card ->
-      Map.get(signs_map, card)
+  defp hand_with_j(cards) do
+    cards
+    |> Enum.frequencies()
+    |> Map.pop("J", 0)
+    |> then(fn {j, rest} ->
+      rest
+      |> Map.values()
+      |> Enum.sort(:desc)
+      |> case do
+        [] -> [5]
+        [top | rest] -> [top + j | rest]
+      end
     end)
+  end
+
+  defp sort_hands(hands) do
+    hands
+    |> Enum.sort_by(fn {_, kicker, _} -> kicker end, :asc)
+    |> Enum.sort_by(fn {hand, _, _} -> hand end, :asc)
+  end
+
+  defp sum_bids(hands) do
+    hands
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_card, _kicker, bid}, rank} ->
+      bid * rank
+    end)
+    |> Enum.sum()
   end
 
   def part1(input) do
@@ -55,34 +58,8 @@ defmodule AdventOfCode.Day07 do
     |> Enum.map(fn {cards, bid} ->
       {hand(cards), kicker(@signs_map, cards), bid}
     end)
-    |> Enum.sort_by(fn {_, kicker, _} -> kicker end, :asc)
-    |> Enum.sort_by(fn {hand, _, _} -> Map.get(@hands, hand) end, :asc)
-    |> Enum.with_index(1)
-    |> Enum.map(fn {{_card, _kicker, bid}, rank} ->
-      bid * rank
-    end)
-    |> Enum.sum()
-  end
-
-  defp hand_with_j(cards) do
-    {j, rest} =
-      cards
-      |> Enum.frequencies()
-      |> Map.pop("J", 0)
-
-    rest
-    |> Enum.map(fn {_, v} -> v end)
-    |> Enum.sort(:desc)
-    |> case do
-      # 5 J
-      [] ->
-        [5]
-
-      [top | rest] ->
-        # Add Js to the biggest
-        Enum.reverse([top + j | rest])
-    end
-    |> get_hand()
+    |> sort_hands()
+    |> sum_bids()
   end
 
   def part2(input) do
@@ -90,12 +67,7 @@ defmodule AdventOfCode.Day07 do
     |> Enum.map(fn {cards, bid} ->
       {hand_with_j(cards), kicker(@signs2_map, cards), bid}
     end)
-    |> Enum.sort_by(fn {_, kicker, _} -> kicker end, :asc)
-    |> Enum.sort_by(fn {hand, _, _} -> Map.get(@hands, hand) end, :asc)
-    |> Enum.with_index(1)
-    |> Enum.map(fn {{_card, _kicker, bid}, rank} ->
-      bid * rank
-    end)
-    |> Enum.sum()
+    |> sort_hands()
+    |> sum_bids()
   end
 end
