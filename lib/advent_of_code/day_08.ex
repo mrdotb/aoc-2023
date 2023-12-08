c0dcb781e8d5e3c224dccbb0598572c8a09a2a11
mrdotb <mrdotb@protonmail.com>
2023-12-08 13:00:05 +0100

    day 8

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,80 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  defp parse_input(input) do
+    [instructions, steps] = String.split(input, "\n\n")
+
+    instructions = String.split(instructions, "", trim: true)
+
+    map_steps =
+      steps
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn
+        <<path::binary-size(3), " = (", left::binary-size(3), ", ", right::binary-size(3), ")">> ->
+          {path, {left, right}}
+      end)
+      |> Map.new()
+
+    {instructions, map_steps}
   end
 
-  def part2(_args) do
+  def part1(input) do
+    {instructions, map_steps} = parse_input(input)
+
+    instructions
+    |> Stream.cycle()
+    |> Enum.reduce_while({"AAA", 0}, fn
+      _instruction, {"ZZZ", step_count} ->
+        {:halt, step_count}
+
+      instruction, {current_path, step_count} ->
+        side = if(instruction == "L", do: 0, else: 1)
+        next_path = map_steps |> Map.get(current_path) |> elem(side)
+        {:cont, {next_path, step_count + 1}}
+    end)
+  end
+
+  defp lcm(nums) do
+    Enum.reduce(nums, 1, fn n, r -> div(n * r, Integer.gcd(n, r)) end)
+  end
+
+  def part2(input) do
+    {instructions, map_steps} = parse_input(input)
+
+    ghost_entrances =
+      Enum.filter(map_steps, fn
+        {<<_::binary-size(2), "A">>, _} -> true
+        _ -> false
+      end)
+      |> Enum.map(fn {key, _} -> key end)
+
+    instructions
+    |> Stream.cycle()
+    |> Enum.reduce_while({ghost_entrances, [], 0}, fn instruction,
+                                                      {current_steps, steps_count, step_count} ->
+      step_count = step_count + 1
+
+      {finish, current_steps} =
+        Enum.map(current_steps, fn
+          current_step ->
+            side = if(instruction == "L", do: 0, else: 1)
+            map_steps |> Map.get(current_step) |> elem(side)
+        end)
+        |> Enum.split_with(fn
+          <<_::binary-size(2), "Z">> -> true
+          _ -> false
+        end)
+
+      steps_count =
+        case finish do
+          [] -> steps_count
+          [_] -> [step_count | steps_count]
+        end
+
+      if current_steps == [] do
+        {:halt, steps_count}
+      else
+        {:cont, {current_steps, steps_count, step_count}}
+      end
+    end)
+    |> lcm()
   end
 end
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -3,19 +3,50 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input_test """
+  RL
 
-    assert result
+  AAA = (BBB, CCC)
+  BBB = (DDD, EEE)
+  CCC = (ZZZ, GGG)
+  DDD = (DDD, DDD)
+  EEE = (EEE, EEE)
+  GGG = (GGG, GGG)
+  ZZZ = (ZZZ, ZZZ)
+  """
+
+  @input_test_2 """
+  LR
+
+  11A = (11B, XXX)
+  11B = (XXX, 11Z)
+  11Z = (11B, XXX)
+  22A = (22B, XXX)
+  22B = (22C, 22C)
+  22C = (22Z, 22Z)
+  22Z = (22B, 22B)
+  XXX = (XXX, XXX)
+  """
+
+  test "part1 test input" do
+    result = part1(@input_test)
+
+    assert result == 2
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "part1 test" do
+    input = AdventOfCode.Input.get!(8, 2023)
+    result = part1(input)
 
-    assert result
+    assert result == 15517
+  end
+
+  test "part2 test input" do
+    assert part2(@input_test_2) == 6
+  end
+
+  test "part2 test" do
+    input = AdventOfCode.Input.get!(8, 2023)
+    assert part2(input) == 14_935_034_899_483
   end
 end
